842b56827848b10469d836c91fa7aab596ac3a06
Min Yi <openman@gmail.com>
2018-07-10 01:17:15 +0900

    .

--- src/NoteWebApp/Areas/Lab/Controllers/HomeController.cs
+++ src/NoteWebApp/Areas/Lab/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using NoteWebApp.Areas.Lab.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,6 +21,26 @@ namespace NoteWebApp.Areas.Lab.Controllers
 			List<BinaryFileVO> binaryFileVOList = BinaryFile.GetVOList();
 
 			return View(binaryFileVOList);
+		}
+
+		[HttpPost]
+		public ActionResult Upload()
+		{
+			if (Request.Files.Count > 0)
+			{
+				var file = Request.Files[0];
+
+				Debug.WriteLine($"file name: {file.FileName}" );
+
+				//if (file != null && file.ContentLength > 0)
+				//{
+				//	var fileName = Path.GetFileName(file.FileName);
+				//	var path = Path.Combine(Server.MapPath("~/Images/"), fileName);
+				//	file.SaveAs(path);
+				//}
+			}
+
+			return RedirectToAction("SimpleFileUpload");
 		}
     }
 }
